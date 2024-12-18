1ac77e505d683b47a6593fd6616cf60076afcbe8
iconsumeplutonium <uzymc242@outlook.com>
2024-12-17 22:13:09 -0800

    clean up search

--- search.py
+++ search.py
@@ -58,20 +58,16 @@ def bfs(adj):
 
 if __name__ == "__main__":
     parser : argparse.ArgumentParser = argparse.ArgumentParser(description="connects franchise")
-    parser.add_argument("-s", "--start", type=int, help="The franchise to start from")
+    parser.add_argument("-s", "--start", type=str, help="The franchise to start from")
     parser.add_argument("-l", "--min-link", type=int, help="The minimum link type to consider as a valid path")
     args : argparse.Namespace = parser.parse_args()
 
-
     if not args.start:
         print("Error: start is required")
-
-    start: int = args.start
-    minLinkType: int = args.min_link if args.min_link else float('inf')
+        exit(1)
 
     conn : sqlite3.Connection = sqlite3.connect('crossovers.db')
     cursor : sqlite3.Cursor = conn.cursor()
-
     # create a dictionary for easy franchise ID lookup
     idFromName: dict = {}
     nameFromID: dict = {}
@@ -84,143 +80,65 @@ if __name__ == "__main__":
         idFromName[name] = id
         nameFromID[id] = name
 
-    # create an adjacency list representation of all crossovers
-    # adj : DefaultDict[int, List[int]] = defaultdict(list)
-    # cursor.execute("SELECT gameID, COgameID FROM links;")
-    # rows = cursor.fetchall()
-    # for row in rows:
-    #     adj[row[0]].append(row[1])
+    if args.start not in idFromName:
+        print(f"Error: franchise {args.start} does not exist.")
+        exit(2)
+
+    start: str = idFromName[args.start]
+    minLinkType: int = args.min_link if args.min_link else float('inf')
+
+    # adjacency list representation of all crossovers
     with open('text/crossovers.json', 'r') as crossoverJSON:
         adj: dict = json.load(crossoverJSON)
 
 
-    queue: Deque[int] = deque()
-    visited: Set[int] = set()
-    predecessor: Dict[int, int] = {
-        start: -1
-    }
-    #queue.append(idLookup[start])
+    queue: deque[int] = deque()
+    visited: set[int] = set()
+    predecessor: dict[int, int] = {start: -1}
 
     queue.append(start)
     predecessor[start] = -1
     visited.add(start)
 
-    #bfs(adj)
+    print(f"\nSearching for path from {args.start} to Fortnite...")
+    
+    while (len(queue) > 0):
+        qSize : int = len(queue)
+        for i in range(qSize):
+            franchiseID: int = queue.popleft()
+            if franchiseID == FORTNITE:
 
-    print(predecessor)
+                # follow predecessor subgraph to extract path from Fortnite back to start
+                path: list[int] = []
+                f: int = franchiseID
+                while f != -1:
+                    path.append(f)
+                    f = predecessor[f]
+                path.reverse()
 
-    useMultiplePaths : bool = False
-    if useMultiplePaths:
-        allPaths = []
-        path = []
-        dfs(start, adj, path)
-        print(path)
-        allPaths.append(path)
-        path.pop()
+                print(f"Path to Fortnite found: {path}\n")
+                print(args.start)
 
-        disallowed = []
-        for i in range(len(path) - 1, -1, -1):
-            queue : Deque[int] = deque()
-            visited : Set[int] = set()
-            predecessor : Dict[int, int] = dict()
+                # print path in readable format
+                for i in range(1, len(path)):
+                    cursor.execute(f"SELECT crossoverDate, description FROM links WHERE gameID = {path[i - 1]} AND COgameID = {path[i]};")
+                    crossoverInfo : list[tuple] = cursor.fetchall()[0]
 
-            start = path[i]
-            queue.append(start)
-            predecessor[start] = -1
-            visited.add(start)
-            if i != len(path) - 1:
-                visited.add(path[i + 1])
+                    print(f"\n{nameFromID[path[i]]} ({crossoverInfo[0]})\n {crossoverInfo[1]}")
 
-            # for d in disallowed:
-            #     visited.add(d)
+                exit(1)
 
-            while (len(queue) > 0):
-                qSize : int = len(queue)
-                for i in range(qSize):
-                    franchiseID : int = queue.popleft()
-                    if franchiseID == 1237:
-                        p = []
-                        f : int = franchiseID
-                        while f != -1:
-                            p.append(f)
-                            f = predecessor[f]
-                        p.reverse()
-                        print(f"Path to Fortnite found: {p}\n")
-                        allPaths.append(p)
-                        disallowed.append(start)
+            franchiseName: str = nameFromID[franchiseID]
+            for crossover in adj[franchiseName]:
+                crossoverName: str = crossover["game"]
+                crossoverID: int = idFromName[crossoverName]
 
-                        # cursor.execute(f"SELECT name FROM game WHERE id = {path[0]};")
-                        # g1 = cursor.fetchall()[0][0]
-                        # print(g1)
-
-                        # for i in range(1, len(path)):
-                        #     cursor.execute(f"SELECT crossoverDate, description FROM links WHERE gameID = {path[i - 1]} AND COgameID = {path[i]};")
-                        #     coInfo : list[tuple] = cursor.fetchall()[0]
-                            
-                            
-                        #     cursor.execute(f"SELECT name FROM game WHERE id = {path[i]};")
-                        #     g2 = cursor.fetchall()[0][0]
+                if crossoverID in visited: continue
+                if crossover["linkType"] > minLinkType: continue
 
 
-                        #     print(f"\n{g2} ({coInfo[0]})\n {coInfo[1]}")
+                queue.append(crossoverID)
+                visited.add(crossoverID)
+                predecessor[crossoverID] = franchiseID
 
-                        # exit(1)
-
-                    for crossover in adj[franchiseID]:
-                        if crossover in visited: continue
-
-                        queue.append(crossover)
-                        visited.add(crossover)
-                        predecessor[crossover] = franchiseID
-
-
-        print(allPaths)
-    else:
-        while (len(queue) > 0):
-            qSize : int = len(queue)
-            for i in range(qSize):
-                franchiseID: int = queue.popleft()
-                if franchiseID == FORTNITE:
-                    path: list[int] = []
-                    f: int = franchiseID
-                    while f != -1:
-                        path.append(f)
-                        print(f"appending {f}")
-                        f = predecessor[f]
-                    path.reverse()
-                    print(f"Path to Fortnite found: {path}\n")
-
-                    cursor.execute(f"SELECT name FROM game WHERE id = {path[0]};")
-                    g1 = cursor.fetchall()[0][0]
-                    print(g1)
-
-                    for i in range(1, len(path)):
-                        cursor.execute(f"SELECT crossoverDate, description FROM links WHERE gameID = {path[i - 1]} AND COgameID = {path[i]};")
-                        coInfo : list[tuple] = cursor.fetchall()[0]
-                        
-                        
-                        cursor.execute(f"SELECT name FROM game WHERE id = {path[i]};")
-                        g2 = cursor.fetchall()[0][0]
-
-
-                        print(f"\n{g2} ({coInfo[0]})\n {coInfo[1]}")
-
-                    exit(1)
-
-                franchiseName: str = nameFromID[franchiseID]
-                print(start in visited)
-                for crossover in adj[franchiseName]:
-                    crossoverName: str = crossover["game"]
-                    crossoverID: int = idFromName[crossoverName]
-
-                    if crossoverID in visited: continue
-                    if crossover["linkType"] > minLinkType: continue
-
-
-                    queue.append(crossoverID)
-                    visited.add(crossoverID)
-                    predecessor[crossoverID] = franchiseID
-
-        cursor.execute(f"SELECT name FROM game WHERE id = {start};")
-        name : str = cursor.fetchall()[0][0]
-        print(f"no path could be found from {name} to Fortnite")
+    print(f"No path could be found from {args.start} to Fortnite")
