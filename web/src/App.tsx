85bfb423833d98a16799ce8e34773dd759e6aa86
iconsumeplutonium <uzymc242@outlook.com>
2024-12-22 19:39:51 -0800

    some basic css for the input box

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
 	const [inputQuery, setInput] = useState('');
-	const [_selectedFranchise, setSelectedFranchise] = useState('')
+	const [selectedFranchise, setSelectedFranchise] = useState('')
 	const [franchiseList, setFranchiseList] = useState<string[]>([]);
 
-	const aSet = useMemo(() => new Set(franchiseList), [franchiseList]);
+	const franchiseSet = useMemo(() => new Set(franchiseList), [franchiseList]);
 
 	useEffect(() => {
 		const readFileIntoArray = async () => {
@@ -25,9 +25,16 @@ function App() {
 	}, []);
 
 	const filterFranchises = () => {
+		let regexSearch;
+		try {
+			regexSearch = new RegExp(`^${inputQuery}`, "i");
+		} catch (error) {
+			//if the regex fails (e.g a \ at the end of inputQuery), dont bother trying to match
+			return;
+		}
+
 		return Array.from(franchiseList)
 			.filter((option: string) => {
-				const regexSearch = new RegExp(`^${inputQuery}`, "i");
 				return regexSearch.test(option);
 			})
 			.map((option: string, index: number) => {
@@ -35,41 +42,44 @@ function App() {
 			})
 	}
 
-	const validateSelection = (input: string) => {
-		if (aSet.has(input)) {
-			console.log("Selection:", input);
-			setSelectedFranchise(input);
-		}
-	}
 
 	return (
 		<>
 			<div>
 				<h1>Six Degrees of Fortnite</h1>
-				{"Connect "}
-				<input
-					type="text"
-					list="franchise"
-					value={inputQuery}
-					onChange={(e) => { setInput(e.target.value) }}
-					onKeyDown={(e) => {
-						if (e.key === 'Enter') {
-							validateSelection(inputQuery)
-						}
-					}}
-					onInput={(e) => {
-						const test = e.target as HTMLInputElement;
-						validateSelection(test.value);
-					}}
-					placeholder="Search..."
-				/>
-				<datalist id="franchise">
-					{inputQuery && filterFranchises()}
-				</datalist>
-				{" to Fortnite"}
+				<div>
+					<label className="connectText">
+						{"Connect "}
+					</label>
+					<input
+						className="inputBox"
+						type="text"
+						list="franchise"
+						value={inputQuery}
+						onChange={(e) => { setInput(e.target.value) }}
+						onKeyDown={(e) => {
+							if (e.key === 'Enter' && franchiseSet.has(inputQuery))
+								setSelectedFranchise(inputQuery);
+						}}
+						onInput={(e) => {
+							const test = e.target as HTMLInputElement;
+							if (franchiseSet.has(test.value))
+								setSelectedFranchise(test.value);
+						}}
+						placeholder="Search..."
+					/>
+					<datalist id="franchise">
+						{inputQuery && filterFranchises()}
+					</datalist>
+					<label className='connectText'>
+						{" to Fortnite"}
+					</label>
+				</div>
 			</div>
-			<br />			
-			<button>
+			<br />
+			<button
+				onClick={() => { console.log(selectedFranchise) }}
+			>
 				Go!
 			</button>
 		</>
