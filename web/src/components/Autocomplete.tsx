99cc765ba65696615f9fc69365edc56c40780f9c
iconsumeplutonium <uzymc242@outlook.com>
2025-02-18 18:18:56 -0800

    mostly functional autocomplete

---
+++ web/src/components/Autocomplete.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react"
+import './Autocomplete.css'
+
+const MAX_RESULTS = 10;
+
+interface AutocompleteProps {
+    data: string[],            //the list of franchises
+    currentValue: string,      //the current text typed in the textbox (react state variable)
+    onCurrentValueChange: (newValue?: string) => void,      //the function that will change currentValue (react state function thing)
+}
+
+function filterFranchises(franchiseList: string[], inputQuery: string, selectedIndex: number) {
+    // if (franchiseList.length == 0) (<div></div>);
+    if (!inputQuery || inputQuery.length == 0) return [];
+
+    let regexSearch;
+    try {
+        regexSearch = new RegExp(`^${inputQuery}`, "i");
+    } catch (error) {
+        //if the regex fails (e.g a \ at the end of inputQuery), dont bother trying to match
+        return [];
+    }
+
+    return Array.from(franchiseList)
+        .filter((option: string) => {
+            return regexSearch.test(option);
+        })
+        .slice(0, MAX_RESULTS);
+}
+
+const Autocomplete: React.FC<AutocompleteProps> = ({ data, currentValue, onCurrentValueChange }) => {
+    const [isFocus, setIsFocus] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+
+    return (
+        <div className="autocompleteParent">
+            <input
+                className="inputBox"
+                type="text"
+                list="franchise"
+                value={currentValue}
+                onChange={(e) => {
+                    const a = e.target.value;
+                    onCurrentValueChange(a);
+                    setSelectedIndex(0); //reset selection to unselected
+                    setSuggestions(filterFranchises(data, a, selectedIndex));
+                }}
+                onFocus={() => (setIsFocus(true))}
+                onBlur={() => (setIsFocus(false))}
+                onKeyDown={(e) => {
+                    switch (e.key) {
+                        case 'ArrowUp':
+                            setSelectedIndex(prevIndex => {
+                                const newIndex = Math.max(prevIndex - 1, 0);
+                                setSuggestions(filterFranchises(data, currentValue, newIndex));
+                                return newIndex;
+                            })
+                            break;
+                        case 'ArrowDown':
+                            setSelectedIndex(prevIndex => {
+                                const newIndex = Math.min(prevIndex + 1, suggestions.length - 1);
+                                setSuggestions(filterFranchises(data, currentValue, newIndex));
+                                return newIndex;
+                            });
+                            break;
+                        case 'Enter':
+                            if (selectedIndex < 0) break;
+                            console.log(suggestions[selectedIndex]);
+                            onCurrentValueChange(suggestions[selectedIndex]);
+                            setIsFocus(false);
+                            break;
+                    }
+                    console.log(selectedIndex)
+                }}
+                placeholder="Search..."
+            />
+
+            {isFocus && (currentValue.length > 0) && <div className="resultsList">
+                {suggestions.map((option: string, index: number) => {
+                    return (
+                        <div
+                            key={option}
+                            className="option"
+                            onMouseDown={(e) => {
+                                e.preventDefault();
+                                console.log(option);
+                            }}
+                            style={index == selectedIndex ? { backgroundColor: "#0000FF" } : {}}
+                        >
+                            {option}
+                        </div>)
+                })}
+            </div>}
+        </div>
+    );
+}
+
+export default Autocomplete;
