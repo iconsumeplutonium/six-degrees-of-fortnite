9c1aa417d70664068d9c868b61001fc7be034ddf
iconsumeplutonium <uzymc242@outlook.com>
2025-07-08 12:29:11 -0700

    attempt to fix favicon issue

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import './index.css'
 import App from './App.tsx'
 import CrossoverGraphThree from './visualizer/CrossoverGraphThree.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
