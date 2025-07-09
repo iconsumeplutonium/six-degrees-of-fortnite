cbcdc2176eb0a836741de0496991e08d0f75deaf
iconsumeplutonium <uzymc242@outlook.com>
2025-07-08 19:52:28 -0700

    add attribution at bottom of main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,9 @@ import { useNavigate, useSearchParams } from 'react-router';
 import Autocomplete from './components/Autocomplete';
 import Timeline from './components/Timeline';
 import Navigation from './components/Navigation';
+import Attribution from './components/Attribution';
 import { getFranchises } from './hooks/GetFranchiseList';
-import CrossoverHooks from './hooks/GetCrossovers'; 
+import CrossoverHooks from './hooks/GetCrossovers';
 import Logo from './assets/6dof_logo.png';
 import { Path } from './types';
 import './styles/App.css';
@@ -33,46 +34,54 @@ export default function App() {
 	}, [selectedFranchise]);
 
 	return (
-		<>
-			<div className="mainPageLayout">
-				<Navigation />
-				<a href="/">
-					<img className='logo' src={Logo}></img>
-				</a>
-				<div className='searchContainer'>
-					<label className="connectText">
-						{"Connect "}
-					</label>
-					<Autocomplete
-						data={franchiseList}
-						onFranchiseInput={(f: string) => {
-							if (!franchiseSet.has(f)) return;
+		<div style={{
+			minHeight: '100vh',
+			display: 'flex',   /* this stuff and the flex:1 stuff makes it so that the attribution sticks to the bottom of the page if theres overflow, and to the bottom of the viewport if no overflow */
+			flexDirection: 'column'
+		}}>
+			<div style={{ flex: '1' }}>
+				<div className="mainPageLayout">
+					<Navigation />
+					<a href="/">
+						<img className='logo' src={Logo}></img>
+					</a>
+					<div className='searchContainer'>
+						<label className="connectText">
+							{"Connect "}
+						</label>
+						<Autocomplete
+							data={franchiseList}
+							onFranchiseInput={(f: string) => {
+								if (!franchiseSet.has(f)) return;
 
-							navigate(`?source=${encodeURIComponent(f)}`)
+								navigate(`?source=${encodeURIComponent(f)}`)
 
-							// getCrossover is called automatically whenever setSelectedFranchise is called
-							setSelectedFranchise(f);
-						}}
-						prefill={prefill}
-					/>
+								// getCrossover is called automatically whenever setSelectedFranchise is called
+								setSelectedFranchise(f);
+							}}
+							prefill={prefill}
+						/>
 
-					<label className='connectText'>
-						{" to Fortnite"}
-					</label>
+						<label className='connectText'>
+							{" to Fortnite"}
+						</label>
+					</div>
 				</div>
+				<button onClick={() => { GetCrossover(selectedFranchise) }} style={{ marginTop: '10px' }} className='goButton'>
+					<b style={{ color: 'black' }}>Go!</b>
+				</button>
+				<br /><br />
+				{
+					isLoading ? "Loading..." :
+						<Timeline
+							selectedFranchise={selectedFranchise}
+							crossoverData={crossoverData}
+							wasAPIError={wasAPIError}
+						/>
+				}
 			</div>
-			<button onClick={() => {GetCrossover(selectedFranchise)}} style={{ marginTop: '10px' }} className='goButton'>
-				<b style={{ color: 'black' }}>Go!</b>
-			</button>
-			<br /><br />
-			{
-				isLoading ? "Loading..." :
-					<Timeline
-						selectedFranchise={selectedFranchise}
-						crossoverData={crossoverData}
-						wasAPIError={wasAPIError}
-					/>
-			}
-		</>
+
+			<Attribution />
+		</div>
 	)
 }
---
+++ web/src/components/Attribution.tsx
@@ -0,0 +1,17 @@
+export default function Attribution() {
+    return (
+        <div style={{ marginTop: '40px', textAlign: 'center' }}>
+            <p style={{
+                fontSize: '12px',
+                color: '#888',
+                lineHeight: '1.3',
+                margin: '0 auto',
+                padding: '4px',
+            }}>
+                All data on this site was pulled from the <a href="https://fictionalcrossover.fandom.com/wiki/Crossover_Wiki">Fictional Crossover Wiki</a>,
+                created and maintained by its community of contributors and authors. This data is licensed under the
+                <a href="https://creativecommons.org/licenses/by-sa/3.0/" target="_blank" rel="noopener noreferrer"> Creative Commons Attribution-Share Alike License 3.0 (Unported) (CC BY-SA)</a>.
+            </p>
+        </div>
+    )
+}
